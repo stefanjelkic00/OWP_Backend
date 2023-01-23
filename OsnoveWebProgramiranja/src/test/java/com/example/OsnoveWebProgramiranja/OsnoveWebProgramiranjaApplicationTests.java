9626d43a7bfa80f131d42186313ed8e4ec5c18af
stefanjelkic00 <stefanjelkic@gmail.com>
2023-01-23 22:51:11 +0100

    Initial commit .

---
+++ OsnoveWebProgramiranja/src/test/java/com/example/OsnoveWebProgramiranja/OsnoveWebProgramiranjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OsnoveWebProgramiranja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsnoveWebProgramiranjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

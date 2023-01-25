575c8f478f23e70856364bd928d2eabcac8e90de
stefanjelkic00 <62428658+stefanjelkic00@users.noreply.github.com>
2023-01-25 08:33:51 +0100

    Changes on models and added model of Delivery Status UTIL (#13)
    Co-authored-by: stefanjelkic00 <stefanjelkic@gmail.com>

--- OsnoveWebProgramiranja/src/main/java/models/Vaccine.java
+++ OsnoveWebProgramiranja/src/main/java/models/Vaccine.java
@@ -1,11 +1,14 @@
 package models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+
+@Entity
 public class Vaccine {
 	
 
